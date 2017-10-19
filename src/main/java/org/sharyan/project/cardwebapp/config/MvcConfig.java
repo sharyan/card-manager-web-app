5bd900a78a018ad243a000f5a1a8c582d9958d39
Shane Ryan <sharyan@cisco.com>
2017-10-19 09:01:20 +0100

    fix mvc config to inherit from mvc config adapter

--- src/main/java/org/sharyan/project/cardwebapp/config/MvcConfig.java
+++ src/main/java/org/sharyan/project/cardwebapp/config/MvcConfig.java
@@ -3,8 +3,9 @@ package org.sharyan.project.cardwebapp.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
-public class MvcConfig {
+public class MvcConfig extends WebMvcConfigurerAdapter {
 }
