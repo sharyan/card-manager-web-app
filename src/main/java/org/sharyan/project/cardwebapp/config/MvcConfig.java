9282360226bc5ef9e412518869f681bbeb055dee
Shane Ryan <sharyan@cisco.com>
2017-10-19 09:39:20 +0100

    add initial view structure

--- src/main/java/org/sharyan/project/cardwebapp/config/MvcConfig.java
+++ src/main/java/org/sharyan/project/cardwebapp/config/MvcConfig.java
@@ -1,11 +1,52 @@
 package org.sharyan.project.cardwebapp.config;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+import org.thymeleaf.templateresolver.TemplateResolver;
 
 @Configuration
 @EnableWebMvc
 public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(final ViewControllerRegistry registry) {
+        super.addViewControllers(registry);
+        registry.addViewController("/").setViewName("forward:/login");
+        registry.addViewController("/login");
+        registry.addViewController("/register.html");
+        registry.addViewController("/logout.html");
+        registry.addViewController("/homepage.html");
+        registry.addViewController("/admin.html");
+    }
+
+    @Bean
+    public TemplateResolver templateResolver() {
+        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver();
+        templateResolver.setPrefix("/WEB-INF/templates");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode("HTML5");
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        return new SpringTemplateEngine();
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine());
+        viewResolver.setOrder(1);
+        viewResolver.setViewNames(new String[]{"*.html"});
+        return viewResolver;
+    }
 }
