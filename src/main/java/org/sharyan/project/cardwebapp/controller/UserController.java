074ccb7f96a56704435dd95972697b0d9b8546a9
Shane Ryan <sharyan@cisco.com>
2017-10-19 08:58:44 +0100

    add initial API surface for user management

---
+++ src/main/java/org/sharyan/project/cardwebapp/controller/UserController.java
@@ -0,0 +1,34 @@
+package org.sharyan.project.cardwebapp.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/user/login")
+    public String getLoginPage() {
+        // TODO: add login view
+        return "login";
+    }
+
+    @PostMapping("/user/login")
+    public String performLogin() {
+        // TODO: add welcome home page
+        return "welcome";
+    }
+
+    @GetMapping("/user/register")
+    public String getRegistrationPage() {
+        // TODO: add registration view
+        return "register";
+    }
+
+    @PostMapping
+    public String doRegistration() {
+        // TODO: add welcome home page
+        return "welcome";
+    }
+}
