82b438ad2ead042b60c5cbadd769f5d10d575cca
Shane Ryan <sharyan@cisco.com>
2017-10-19 08:47:08 +0100

    add skeleton project from spring-boot initializer

---
+++ src/main/java/org/sharyan/project/cardwebapp/CardWebAppApplication.java
@@ -0,0 +1,12 @@
+package org.sharyan.project.cardwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardWebAppApplication.class, args);
+	}
+}
---
+++ src/test/java/org/sharyan/project/cardwebapp/CardWebAppApplicationTests.java
@@ -0,0 +1,16 @@
+package org.sharyan.project.cardwebapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CardWebAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
