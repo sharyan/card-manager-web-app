57c6d03e8b15d6cbfb80aa8b86b6039c73b3882b
Shane Ryan <shaneryan.eng@gmail.com>
2017-10-22 21:03:28 +0100

    add rudimentary validation on payment card dto

--- src/main/java/org/sharyan/project/cardwebapp/dto/PaymentCardDto.java
+++ src/main/java/org/sharyan/project/cardwebapp/dto/PaymentCardDto.java
@@ -3,6 +3,7 @@ package org.sharyan.project.cardwebapp.dto;
 import lombok.Builder;
 import lombok.Value;
 
+import javax.validation.constraints.*;
 import java.time.Month;
 import java.time.Year;
 
@@ -11,8 +12,22 @@ import java.time.Year;
 @Builder
 public class PaymentCardDto {
 
+    @NotNull
+    @NotBlank
+    @NotEmpty
+    @Pattern(regexp = "[0-9]{13,19}") // basic check for card numbers, this would be more elaborate for a 'real' payment system
     private final String cardNumber;
+
+    @NotNull
+    @NotBlank
+    @NotEmpty
+    @Pattern(regexp = "[A-Za-z0-9]*")
+    @Size(min = 3, max = 30)
     private final String cardName;
+
+    @FutureOrPresent
     private final Month expiryMonth;
+
+    @FutureOrPresent
     private final Year expiryYear;
 }
