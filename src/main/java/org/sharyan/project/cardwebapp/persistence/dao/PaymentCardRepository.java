b364e2e9d50bb003c275151099918cafdd5e20a2
Shane Ryan <shaneryan.eng@gmail.com>
2017-10-22 19:50:18 +0100

    add payment card jpa repository adapter

---
+++ src/main/java/org/sharyan/project/cardwebapp/persistence/dao/PaymentCardRepository.java
@@ -0,0 +1,16 @@
+package org.sharyan.project.cardwebapp.persistence.dao;
+
+import org.sharyan.project.cardwebapp.persistence.entity.PaymentCard;
+import org.sharyan.project.cardwebapp.persistence.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PaymentCardRepository extends JpaRepository<PaymentCard, Long> {
+
+    PaymentCard findByCardNumber(String cardNumber);
+
+    List<PaymentCard> findAllByCardNumberEquals(String cardNumber);
+
+    PaymentCard findAllByOwner(User owner);
+}
