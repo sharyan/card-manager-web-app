212e29704a0d7847d21c08b4f3e26138b8ccd746
Shane Ryan <shaneryan.eng@gmail.com>
2017-10-23 11:44:33 +0100

    add error messages on PaymentCard vaidation

--- src/main/java/org/sharyan/project/cardwebapp/dto/PaymentCardDto.java
+++ src/main/java/org/sharyan/project/cardwebapp/dto/PaymentCardDto.java
@@ -7,27 +7,21 @@ import javax.validation.constraints.*;
 import java.time.Month;
 import java.time.Year;
 
-// TODO: add validation
 @Value(staticConstructor = "createNewCard")
 @Builder
 public class PaymentCardDto {
 
-    @NotNull
-    @NotBlank
-    @NotEmpty
-    @Pattern(regexp = "[0-9]{13,19}") // basic check for card numbers, this would be more elaborate for a 'real' payment system
+    @NotBlank(message = "Card number cannot be empty")
+    @Pattern(regexp = "[0-9]{13,19}", message = "Card number must only contain numbers") // basic check for card numbers, this would be more elaborate for a 'real' payment system
     private final String cardNumber;
 
-    @NotNull
-    @NotBlank
-    @NotEmpty
-    @Pattern(regexp = "[A-Za-z0-9]*")
+    @NotBlank(message = "Card name cannot be empty")
+    @Pattern(regexp = "[A-Za-z0-9]*", message = "Card namne must be made up of valid alphanumeric characters")
     @Size(min = 3, max = 30)
     private final String cardName;
 
-    @FutureOrPresent
     private final Month expiryMonth;
 
-    @FutureOrPresent
+    @FutureOrPresent(message = "Expiry date must be set in the future")
     private final Year expiryYear;
 }
